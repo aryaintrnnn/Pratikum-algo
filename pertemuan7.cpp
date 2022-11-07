d7f4e0e11d395d4d241daf60fa95fa9e92f99398
INTRN <101205974+Lvthannn@users.noreply.github.com>
2022-11-07 08:29:23 +0800

    pertemuan7

---
+++ pertemuan7.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int deret[5]={2,3,4,5,6};
+    
+    cout<<"Index ke 1 ="<<endl;
+    cout<< deret[1] <<endl;
+    
+
+    return 0;
+}
